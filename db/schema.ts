1d83d685bcabae152d6e06788709a34faec541dc
jb <jbicks.haysr@passinbox.com>
2024-10-23 22:44:55 +0000

    Define a basic user containing only a name and UUID

---
+++ db/schema.ts
@@ -0,0 +1,7 @@
+// db/schema.ts
+import { pgTable, uuid, text } from "drizzle-orm/pg-core";
+
+export const users = pgTable('users', {
+    id: uuid('id').defaultRandom().primaryKey(),
+    username: text('username').notNull(),
+});
