613cd948a441f1fc5453e80561875d554d266b5e
jb <jbicks.haysr@passinbox.com>
2024-10-23 23:17:16 +0000

    Implement userlist view.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 // src/index.ts
+import { neon } from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/neon-http';
 import { Hono } from 'hono';
+import { users } from './db/schema';
 
 export type Env = {
 	DATABASE_URL: string;
@@ -7,8 +10,21 @@ export type Env = {
 
 const app = new Hono<{ Bindings: Env }>();
 
-app.get('/users', (c) => {
-	return c.json({});
+app.get('/users', async (c) => {
+	try {
+		const sql = neon(c.env.DATABASE_URL);
+		const db = drizzle(sql);
+		const result = await db.select().from(users);
+
+		return c.json({result});
+	} catch (error) {
+		return c.json(
+			{
+				error,
+			},
+			500
+		);
+	}
 });
 
 export default app;
