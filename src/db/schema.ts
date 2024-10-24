8edf663c998c0ffb2f50c8435c9bae165fd5e65f
jb <jbicks.haysr@passinbox.com>
2024-10-24 19:34:53 +0000

    Add a DB table for friend tracking.

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,7 +1,22 @@
 // src/db/schema.ts
-import { pgTable, uuid, text } from "drizzle-orm/pg-core";
+import { sql } from "drizzle-orm";
+import { pgTable, uuid, text, primaryKey, check } from "drizzle-orm/pg-core";
 
 export const users = pgTable('users', {
     id: uuid('id').defaultRandom().primaryKey(),
     username: text('username').notNull(),
 });
+
+export const friends = pgTable('friends', {
+    user_id_1: uuid('user_id_1'),
+    user_id_2: uuid('user_id_2')
+}, (table) => ({
+        pk: primaryKey({
+            columns: [table.user_id_1, table.user_id_2 ]
+        }),
+        checkConstraint: check(
+            "id_order_check",
+            sql`${table.user_id_1} < ${table.user_id_2}`
+        ),
+    })
+);
