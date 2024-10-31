dbb44507fc36ee3c2ea5c6f15f3d58408999596a
jb <jbicks.haysr@passinbox.com>
2024-10-31 04:03:06 +0000

    Move test user creation and deletion up to the index spec.

--- test/api/auth.spec.ts
+++ test/api/auth.spec.ts
@@ -2,26 +2,11 @@ import { afterAll, beforeAll, describe, expect, it } from "vitest";
 import worker from "../../src/index";
 import { createExecutionContext, env, waitOnExecutionContext } from "cloudflare:test";
 import { LoginPostResponse } from "../../src/api/auth";
-import { neon } from "@neondatabase/serverless";
 import { decode } from "hono/utils/jwt/jwt";
-
-const testCreds = {
-    username: 'testuser',
-    password: 'testpassword',
-}
-
-const IncomingRequest = Request<unknown, IncomingRequestCfProperties>;
+import { createTestUser, destroyTestUser, IncomingRequest, testCreds } from "../index.spec";
 
 describe('Login Endpoint', () => {
-    beforeAll(async () => {
-        const request = new IncomingRequest('http://example.com/users', {
-            method: "POST",
-            body: JSON.stringify(testCreds),
-        });
-        const ctx = createExecutionContext();
-        const response = await worker.fetch(request, env, ctx);
-        await waitOnExecutionContext(ctx);
-    });
+    beforeAll(createTestUser);
     it('Should accept valid credentials and provide a token.', async () => {
         const request = new IncomingRequest('http://example.com/auth/login', {
             method: 'POST',
@@ -32,23 +17,8 @@ describe('Login Endpoint', () => {
         await waitOnExecutionContext(ctx);
         expect(response.status).toBe(200);
         const body = await response.json() as LoginPostResponse;
-        const payload = await decode(body.token).payload;
+        const payload = decode(body.token).payload;
         expect(payload.username).toBe(testCreds.username);
     });
-    afterAll(async () => {
-        const loginRequest = new IncomingRequest('http://example.com/auth/login', {
-            method: 'POST',
-            body: JSON.stringify(testCreds),
-        });
-        const ctx = createExecutionContext();
-        const response = await worker.fetch(loginRequest, env, ctx);
-        const body = await response.json() as LoginPostResponse;
-        const payload = await(decode(body.token)).payload;
-        const deleteRequest = new IncomingRequest('http://example.com/users/' + payload.id, {
-            method: 'DELETE',
-            body: JSON.stringify({token: body.token})
-        });
-        worker.fetch(deleteRequest, env, ctx);
-        await waitOnExecutionContext(ctx);
-    });
+    afterAll(destroyTestUser);
 });
--- test/index.spec.ts
+++ test/index.spec.ts
@@ -2,8 +2,42 @@
 import { createExecutionContext, env, waitOnExecutionContext } from 'cloudflare:test';
 import { describe, it, expect } from 'vitest';
 import worker from '../src/index';
+import { decode } from 'hono/utils/jwt/jwt';
+import { LoginPostResponse } from '../src/api/auth';
 
-const IncomingRequest = Request<unknown, IncomingRequestCfProperties>;
+export const testCreds = {
+    username: 'testuser',
+    password: 'testpassword',
+}
+
+export const IncomingRequest = Request<unknown, IncomingRequestCfProperties>;
+
+export const createTestUser = async () => {
+	const request = new IncomingRequest('http://example.com/users', {
+		method: "POST",
+		body: JSON.stringify(testCreds),
+	});
+	const ctx = createExecutionContext();
+	await worker.fetch(request, env, ctx);
+	await waitOnExecutionContext(ctx);
+};
+
+export const destroyTestUser = async () => {
+	const loginRequest = new IncomingRequest('http://example.com/auth/login', {
+		method: 'POST',
+		body: JSON.stringify(testCreds),
+	});
+	const ctx = createExecutionContext();
+	const response = await worker.fetch(loginRequest, env, ctx);
+	const body = await response.json() as LoginPostResponse;
+	const payload = decode(body.token).payload;
+	const deleteRequest = new IncomingRequest('http://example.com/users/' + payload.id, {
+		method: 'DELETE',
+		body: JSON.stringify({token: body.token})
+	});
+	worker.fetch(deleteRequest, env, ctx);
+	await waitOnExecutionContext(ctx);
+};
 
 describe('Index', () => {
 	it('Contains no content.', async () => {
@@ -11,6 +45,6 @@ describe('Index', () => {
 		const ctx = createExecutionContext();
 		const response = await worker.fetch(request, env, ctx);
 		await waitOnExecutionContext(ctx);
-		expect(await response.status).toBe(404);
+		expect(response.status).toBe(404);
 	});
 });
